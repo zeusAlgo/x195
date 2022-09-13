07f4fa13ae924fd15d57e76934f1eae22a3ba034
u <uuu@hotmail.com>
2022-09-13 18:46:30 -0500

    .

--- src/main/java/com/example/x195/HelloController.java
+++ src/main/java/com/example/x195/HelloController.java
@@ -1,10 +1,13 @@
 package com.example.x195;
-
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 
 public class HelloController {
+    public TextField usrTxtFld;
+    public PasswordField passTxtFld;
     @FXML
     private Label welcomeText;
 
@@ -15,6 +18,10 @@ public class HelloController {
     @FXML Label geoLbl;
     @FXML Button signinBtn;
     @FXML protected void printHi() {
+        String usrcreds = usrTxtFld.getText();
+        String passcreds = passTxtFld.getText();
+
         System.out.println("Hi");
     }
+    
 }
