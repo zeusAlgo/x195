db9e072bc85d8eef5ed94b1a85a3237d65030211
u <uuu@hotmail.com>
2022-09-14 15:06:22 -0500

    .

---
+++ src/main/java/com/example/x195/customers.java
@@ -0,0 +1,4 @@
+package com.example.x195;
+
+public class customers {
+}
--- src/main/java/com/example/x195/home.java
+++ src/main/java/com/example/x195/home.java
@@ -1,5 +1,5 @@
 package com.example.x195;
-
+import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -12,12 +12,18 @@ public class home {
 
     public void launchActivity(String activityname){
         try {
-            FXMLLoader fxmlLoader = new FXMLLoader(home.class.getResource("home.fxml"));
+            FXMLLoader fxmlLoader = new FXMLLoader(home.class.getResource(activityname+"home.fxml"));
             Scene scene = new Scene(fxmlLoader.load(), 1100, 1100);
             Stage stage = new Stage();
-            stage.setTitle("Home");
+            stage.setTitle(activityname);
             stage.setScene(scene);
             stage.show();
         } catch(Exception e) {System.out.println(e.getMessage());}
     }
+
+    public void launchA(ActionEvent actionEvent) { launchActivity("customers");}
+
+    public void launchB(ActionEvent actionEvent) { launchActivity("appointments");}
+
+    public void launchC(ActionEvent actionEvent) { launchActivity("reports");}
 }
