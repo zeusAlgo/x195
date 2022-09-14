e22518b1aaec5fced5a3aa3fb7696d071ea2a60d
u <uuu@hotmail.com>
2022-09-14 03:11:06 -0500

    .

--- src/main/java/com/example/x195/HelloController.java
+++ src/main/java/com/example/x195/HelloController.java
@@ -13,16 +13,18 @@ import com.mysql.jdbc.Driver;
 public class HelloController {
     public TextField usrTxtFld;
     public PasswordField passTxtFld;
-
     @FXML Label geoLbl;
     @FXML Button signinBtn;
+
     @FXML protected void signin() throws SQLException, ClassNotFoundException {
         String usrcreds = usrTxtFld.getText();
         String passcreds = passTxtFld.getText();
 
-        Class.forName("com.mysql.jdbc.Driver");
+//        Class.forName("com.mysql.jdbc.Driver");
 
-        String db = "jbdc:mysql://localhost:3306/client_schedule";
-        Connection connection = DriverManager.getConnection(db, usrcreds, passcreds);
+        String db = "jbdc:mysql://localhost/client_schedule";
+
+        Connection connection;
+        connection = DriverManager.getConnection(db, usrcreds, passcreds);
     }
 }
