447a9d3119d7feefcc0b4b15f4706af7f04fb53c
u <uuu@hotmail.com>
2022-09-14 12:25:28 -0500

    .

--- src/main/java/com/example/x195/HelloController.java
+++ src/main/java/com/example/x195/HelloController.java
@@ -1,12 +1,14 @@
 package com.example.x195;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 import javafx.scene.control.*;
-
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.*;
 import java.sql.DriverManager;
 import java.time.ZoneId;
-import java.util.Arrays;
 import java.util.Locale;
 
 public class HelloController {
@@ -16,15 +18,18 @@ public class HelloController {
     @FXML Button signinBtn;
     public String lang = "";
 
-    @FXML protected void signin() throws SQLException {
+    @FXML protected void signin(Stage stage) throws SQLException {
         String usrcreds = usrTxtFld.getText();
         String passcreds = passTxtFld.getText();
         String db = "jdbc:mysql://localhost/client_schedule";
         try {
             Connection connection = DriverManager.getConnection(db, usrcreds, passcreds);
-            System.out.println("Connected to db");
-
-        } catch (SQLException e) {
+            FXMLLoader fxmlLoader = new FXMLLoader(home.class.getResource("home.fxml"));
+            Scene scene = new Scene(fxmlLoader.load(), 1100, 1100);
+            stage.setTitle("Home");
+            stage.setScene(scene);
+            stage.show();
+        } catch (SQLException | IOException e) {
             System.out.println("Error" + e.getMessage());
             String alerts = "";
             if (lang.equals("English")) alerts = "Please enter valid username and password";
