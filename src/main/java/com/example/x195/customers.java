07227f68b1716551fe97578325b5a0b274d347a9
u <uuu@hotmail.com>
2022-09-15 20:28:31 -0500

    m

--- src/main/java/com/example/x195/customers.java
+++ src/main/java/com/example/x195/customers.java
@@ -1,14 +1,19 @@
 package com.example.x195;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class customers {
+    ObservableList<String> ar = FXCollections.observableArrayList();
+
 
 
     public void launchActivity(String activityname){
+
         try {
             FXMLLoader fxmlLoader = new FXMLLoader(home.class.getResource(activityname+".fxml"));
             Scene scene = new Scene(fxmlLoader.load(), 1100, 1100);
@@ -18,13 +23,9 @@ public class customers {
             stage.show();
         } catch(Exception e) {System.out.println(e.getMessage());}
     }
-
     public void launch1(ActionEvent actionEvent) { launchActivity("customeradd");}
+    public void laundh2(ActionEvent actionEvent) { launchActivity("customermodify");}
+    public void launch3(ActionEvent actionEvent) {}
 
-    public void laundh2(ActionEvent actionEvent) { launchActivity("customermodify");
-    }
 
-    public void launch3(ActionEvent actionEvent) {
-
-    }
 }
