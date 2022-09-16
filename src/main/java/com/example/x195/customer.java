252cc96b2ffbc3e2cb19985af2172cea549f56de
u <uuu@hotmail.com>
2022-09-16 04:32:43 -0500

    m

--- src/main/java/com/example/x195/customer.java
+++ src/main/java/com/example/x195/customer.java
@@ -1,9 +1,15 @@
 package com.example.x195;
 
 public class customer {
-    String postalcode; String address; int id; String name;
+    int divid;String postalcode; String address; int id; String name; String phone;
 
     public customer(int id, String name, String address, String postalcode, String phone, int divid){
-        this.id = id; this.name = name; this.address=address; this.postalcode=postalcode;
+        this.id = id; this.name = name; this.address=address; this.postalcode=postalcode; this.divid=divid;
     }
+
+    public void setid(int id) {this.id=id;}
+    public void setname(String name) {this.name=name;}
+    public void setaddress(String address) {this.address=address;}
+    public void postalcode(String postalcode) {this.postalcode=postalcode;}
+    public void phone(String phone) {this.phone=phone;}
 }
