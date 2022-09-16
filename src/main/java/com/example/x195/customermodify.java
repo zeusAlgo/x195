fbf71d9985986d27274f0b4909ab2c3bfac47008
u <uuu@hotmail.com>
2022-09-16 16:10:33 -0500

    m

--- src/main/java/com/example/x195/customermodify.java
+++ src/main/java/com/example/x195/customermodify.java
@@ -5,14 +5,14 @@ import java.sql.Statement;
 import java.util.HashMap;
 
 public class customermodify {
-    public HashMap<String, String> customerhm = new HashMap<>();
     public HashMap<String, String> getcustomerinfo(int customerid) {
+        HashMap<String, String> customerhm = new HashMap<>();
         try {
             Statement statement = statement = HelloController.connection.createStatement();
 
         } catch (SQLException e) {
 
         }
-
+        return customerhm;
     }
 }
