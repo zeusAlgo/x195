b0679b7f5621e798c63e0f94c7d87a591f9daf7a
u <uuu@hotmail.com>
2022-09-16 16:02:45 -0500

    m

--- src/main/java/com/example/x195/customermodify.java
+++ src/main/java/com/example/x195/customermodify.java
@@ -1,4 +1,18 @@
 package com.example.x195;
 
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.HashMap;
+
 public class customermodify {
+    public HashMap<String, String> customerhm = new HashMap<>();
+    public HashMap<String, String> getcustomerinfo(int customerid) {
+        try {
+            Statement statement = statement = HelloController.connection.createStatement();
+
+        } catch (SQLException e) {
+
+        }
+
+    }
 }
