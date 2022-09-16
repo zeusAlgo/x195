4486fc0d1da74d5ea101658f07823ccf6f629ad6
u <uuu@hotmail.com>
2022-09-15 20:46:53 -0500

    m

--- src/main/java/com/example/x195/customer.java
+++ src/main/java/com/example/x195/customer.java
@@ -1,13 +1,9 @@
 package com.example.x195;
 
-public class customer() {
-    String postalcode;
-    String address;
-    int id;
-    String name;
+public class customer {
+    String postalcode; String address; int id; String name;
 
     public customer(int id, String name, String address, String postalcode, String phone, int divid){
-        this.id = id;
-        this.name = name; this.address=address; this.postalcode=postalcode;
+        this.id = id; this.name = name; this.address=address; this.postalcode=postalcode;
     }
 }
