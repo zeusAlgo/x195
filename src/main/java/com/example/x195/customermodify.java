c0b971c2013bfb27797672f66ada874ae07ad81f
u <uuu@hotmail.com>
2022-09-16 16:20:21 -0500

    m

--- src/main/java/com/example/x195/customermodify.java
+++ src/main/java/com/example/x195/customermodify.java
@@ -1,5 +1,6 @@
 package com.example.x195;
 
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.HashMap;
@@ -9,10 +10,16 @@ public class customermodify {
         HashMap<String, String> customerhm = new HashMap<>();
         try {
             Statement statement = statement = HelloController.connection.createStatement();
-
-        } catch (SQLException e) {
-
-        }
+            ResultSet res = statement.executeQuery(
+                    "SELECT * FROM customers where Customer_ID="+ customerid);
+            while (res.next()) {
+                customerhm.put("id", String.valueOf(res.getInt("Customer_ID")));
+                customerhm.put("name", res.getString("Customer_Name"));
+                customerhm.put("address", res.getString("Address"));
+                customerhm.put("postalcode", res.getString("Postal_Code"));
+                customerhm.put("")
+            }
+        } catch (SQLException e) { System.out.println("SQL Error: " + e.getMessage()); }
         return customerhm;
     }
 }
