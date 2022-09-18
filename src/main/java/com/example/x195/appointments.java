f53d0799da2fd175b98c7e2e6c71169e9a34c32b
u <uuu@hotmail.com>
2022-09-18 15:21:56 -0500

    m

--- src/main/java/com/example/x195/appointments.java
+++ src/main/java/com/example/x195/appointments.java
@@ -1,11 +1,9 @@
 package com.example.x195;
-
 import javafx.scene.control.TextField;
 
 public class appointments {
+    public TextField modifytxtfld, deltxtfld;
 
 
-    public TextField modifytxtfld;
-    public TextField deltxtfld;
     public void 
 }
