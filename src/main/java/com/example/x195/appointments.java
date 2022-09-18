d36591b0dd7386a213b42b625b196dd8daa297db
u <uuu@hotmail.com>
2022-09-18 15:25:19 -0500

    m

--- src/main/java/com/example/x195/appointments.java
+++ src/main/java/com/example/x195/appointments.java
@@ -1,9 +1,12 @@
 package com.example.x195;
+import javafx.event.ActionEvent;
 import javafx.scene.control.TextField;
 
 public class appointments {
     public TextField modifytxtfld, deltxtfld;
 
+    public void launchA(ActionEvent actionEvent) {home.launchActivity("aptadd");}
+    public void launchB(ActionEvent actionEvent) {home.launchActivity("aptmmodify");}
 
-    public void 
+    
 }
