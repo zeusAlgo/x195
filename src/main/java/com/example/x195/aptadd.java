13bf6f8044332dd30605044651eec55f9dfd1010
u <uuu@hotmail.com>
2022-09-18 18:16:28 -0500

    m

--- src/main/java/com/example/x195/aptadd.java
+++ src/main/java/com/example/x195/aptadd.java
@@ -1,4 +1,10 @@
 package com.example.x195;
 
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+
 public class aptadd {
+    public ComboBox usercombobox, apttimecombobox, customercombobox, contactcombobox;
+    public TextField titletxtfld, desctxtfld, loctxtfld, typetxtfld;
+    
 }
