139a75953305085d0707e24f0d54cda71edc35a9
u <uuu@hotmail.com>
2022-09-17 22:44:18 -0500

    m

--- src/main/java/com/example/x195/customermodify.java
+++ src/main/java/com/example/x195/customermodify.java
@@ -49,7 +49,7 @@ public class customermodify {
             }
             String update = "UPDATE customers set Customer_Name='" + nametxtfld.getText() + "', Address='" + addresstxtfld.getText() + "', Postal_Code='" + postcodetxtfld.getText()+ "', Phone='" + phonetxtfld.getText() + "', Division_ID=" + divid  + " WHERE Customer_ID=" + customers.tomodifyid;
             HelloController.connection.createStatement().executeUpdate(update);
-        } catch (SQLException e) {throw new RuntimeException(e);}
+        } catch (SQLException e) {System.out.println(e.getMessage());}
     }
 
     public void initialize() {
