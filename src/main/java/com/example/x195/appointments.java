2a5296aa1ebce42eea11822268766b7283177896
u <uuu@hotmail.com>
2022-09-18 15:34:13 -0500

    m

--- src/main/java/com/example/x195/appointments.java
+++ src/main/java/com/example/x195/appointments.java
@@ -4,9 +4,12 @@ import javafx.scene.control.TextField;
 
 public class appointments {
     public TextField modifytxtfld, deltxtfld;
+    public static int toalterid = 0;
 
     public void launchA(ActionEvent actionEvent) {home.launchActivity("aptadd");}
-    public void launchB(ActionEvent actionEvent) {home.launchActivity("aptmmodify");}
+    public void launchB(ActionEvent actionEvent) {
+        toalterid = Integer.parseInt(modifytxtfld.getText());
+        home.launchActivity("aptmmodify");}
 
 
     public void deleteapt(ActionEvent actionEvent) { home.del("appointments", "Appointment_ID", Integer.parseInt(deltxtfld.getText()));
