5df51af0ead3394869e79aea5c54d8fc72937278
u <uuu@hotmail.com>
2022-09-18 20:15:29 -0500

    m

--- src/main/java/com/example/x195/aptadd.java
+++ src/main/java/com/example/x195/aptadd.java
@@ -2,9 +2,7 @@ package com.example.x195;
 import javafx.event.ActionEvent;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
-
 import java.sql.SQLException;
-import java.sql.Statement;
 
 public class aptadd {
     public ComboBox usercombobox, apttimecombobox, customercombobox, contactcombobox;
@@ -19,8 +17,20 @@ public class aptadd {
     public void addapt(ActionEvent actionEvent) {
         try {
            int colv = home.incrementcolval("appointments");
+           //todo: fix apt time
+           String apttime = (String) apttimecombobox.getSelectionModel().getSelectedItem();
+           int custid = appointments.customershm.get(customercombobox.getSelectionModel().getSelectedItem());
+           int userid = appointments.usershm.get(usercombobox.getSelectionModel().getSelectedItem());
+           int contactid = appointments.contactshm.get(contactcombobox.getSelectionModel().getSelectedItem());
+           
            HelloController.connection.createStatement().execute(
-                   "Insert into appointments values(" + 
+                   "Insert into appointments values(" + colv + ", '" + titletxtfld.getText() +
+                           "', '" + desctxtfld.getText() + "', '" + loctxtfld.getText() + "', '" + 
+                           typetxtfld.getText() + "', '" + apttime + "', '" + apttime + 
+                           "', '" + "2022-08-30 17:02:46" + "', '" + "script" + "', '" +
+                           "2022-08-30 17:02:46" + "', '" + "script" + "', " + custid + ", " +
+                           userid + ", " + contactid + ");"
+                   
            );
            String insert = "Insert into appointments values(" + colv +
         } catch (SQLException e) {System.out.println("SQL Error: " + e.getMessage());}
