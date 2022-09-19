753750813de9b95fa27747f11129a249687854b8
u <uuu@hotmail.com>
2022-09-19 17:58:27 -0500

    m

--- src/main/java/com/example/x195/appointments.java
+++ src/main/java/com/example/x195/appointments.java
@@ -62,7 +62,7 @@ public class appointments {
     public void initialize() {
        contactshm.put("Anika Costa", 1);contactshm.put("Daniel Garcia", 2);contactshm.put("Li Lee", 3);
        contactsar.addAll("Anika Costa", "Daniel Garcia", "Li Lee" );
-       usershm.put("test", 1);usershm.put("admin", 2); usersar.add("test"); usersar.add("admin");
+       usershm.put("test", 1);usershm.put("admin", 2); usersar.addAll("test", "admin");
        timesar.addAll("4 am", "5 am", "6 am", "7 am", "8 am", "9 am", "10 am", "11 am", "12 am", "1 pm", "2 pm", "3 pm", "4 pm", "5 pm", "6 pm", "7 pm", "8 pm", "9 pm", "10 pm", "11 pm");
 
        customershm = getallcustomers();
