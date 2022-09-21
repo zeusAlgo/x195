9f3325314b4e7e132d1b30367acc8aa862e2290b
u <uuu@hotmail.com>
2022-09-21 13:37:16 -0500

    m

--- src/main/java/com/example/x195/aptmodify.java
+++ src/main/java/com/example/x195/aptmodify.java
@@ -3,12 +3,23 @@ import javafx.event.ActionEvent;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.HashMap;
+
 public class aptmodify {
     public ComboBox userComboBox;public TextField titlecombobox;
     public TextField locationcombobox, typecombobox, descriptioncombobox;
     public ComboBox contactcombobox, monthcombox, daycombobox, timescombobox, customercombobox;
 
-    
+    public HashMap<String, String> getaptinfo(int aptid) {
+        HashMap<String, String> apthm = new HashMap<>();
+        try {
+            ResultSet rs = HelloController.connection.createStatement().executeQuery(
+
+            )
+        } catch(SQLException e) {System.out.println(e.getMessage());}
+    }
 
     public void updateapt(ActionEvent actionEvent) {
     }
