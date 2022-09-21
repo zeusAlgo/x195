f84e3b1495cf6be279c246d9cb920dc3f187aac1
u <uuu@hotmail.com>
2022-09-21 14:35:52 -0500

    m

--- src/main/java/com/example/x195/aptmodify.java
+++ src/main/java/com/example/x195/aptmodify.java
@@ -1,15 +1,19 @@
 package com.example.x195;
 import javafx.event.ActionEvent;
+import javafx.scene.control.Alert;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.util.HashMap;
 
 public class aptmodify {
     public TextField titletxtfld, descriptiontxtfld, locationtxtfld, typetxtfld;
-    public ComboBox userComboBox, contactcombobox, monthcombox, daycombobox, timescombobox, customercombobox;
+    public ComboBox userComboBox, contactcombobox, monthcombobox, daycombobox, timescombobox, customercombobox;
 
     public HashMap<String, String> getaptinfo(int aptid) {
         HashMap<String, String> apthm = new HashMap<>();
@@ -32,7 +36,65 @@ public class aptmodify {
 
     public void updateapt(ActionEvent actionEvent) {
     }
+    public boolean isbizopen() {
+        boolean bool = true;
+        int month = monthcombobox.getSelectionModel().getSelectedIndex()+1;
+        int day = (int) daycombobox.getSelectionModel().getSelectedItem();
+        String hrS = (String) apttimecombobox.getSelectionModel().getSelectedItem();
+        int hr = appointments.timeshm.get(hrS);
 
+        ZonedDateTime aptcurzdt = LocalDateTime.of(2022, month, day, hr, 0).atZone(ZoneId.systemDefault());
+        ZonedDateTime aptestzdt = aptcurzdt.withZoneSameInstant(ZoneId.of("America/New_York")),
+                aptutc = aptcurzdt.withZoneSameInstant(ZoneId.of("UTC"));
+        int utchr = aptutc.getHour(), hour = aptestzdt.getHour();
+        String dayofweek = aptestzdt.getDayOfWeek().toString();
+
+        Alert alert = new Alert(Alert.AlertType.INFORMATION, "Business not open"),
+                alert4 = new Alert(Alert.AlertType.INFORMATION, "Closed that day"),
+                alert2 = new Alert(Alert.AlertType.INFORMATION, "Appointment unavailable");
+        if (hour < 8 | hour > 22) {alert.show();return false;}
+        if (dayofweek.equals("SATURDAY") | dayofweek.equals("SUNDAY")) {alert4.show();return false;}
+//        int contactid = appointments.contactshm.get(contactcombobox.getSelectionModel().getSelectedItem());
+        try {
+            ResultSet rs = HelloController.connection.createStatement().executeQuery("Select * from appointments");
+            while (rs.next()) {
+                String time = rs.getString("Start");
+                String time1 = time.substring(11, 12), time2 = time.substring(11, 13);
+                String hr3 = String.valueOf(utchr);
+                if (time1.equals(hr3) | time2.equals(hr3)) {alert2.show();return false;}//perhaps check for contact ids
+            }
+        } catch (SQLException e) {System.out.println("SQL Error: " + e.getMessage());}
+        return bool;
+    }
+
+    // todo: from db utc to est and to usr time
+
+    public void addapt(ActionEvent actionEvent) {
+        try {
+            if (!isbizopen()) return;
+            int colv = home.incrementcolval("appointments", "Appointment_ID");
+            String hrS = (String) apttimecombobox.getSelectionModel().getSelectedItem();
+
+            int month = monthcombobox.getSelectionModel().getSelectedIndex()+1;
+            int day = (int) daycombobox.getSelectionModel().getSelectedItem();
+            int hr = Integer.parseInt(hrS.substring(0, 1));
+            ZonedDateTime aptcurzdt = LocalDateTime.of(2022, month, day, hr, 0).atZone(ZoneId.systemDefault());
+            ZonedDateTime aptutc = aptcurzdt.withZoneSameInstant(ZoneId.of("UTC")), aptutcend = aptutc.plusHours(1);
+            LocalDateTime aptstart1 = aptutc.toLocalDateTime(), aptend1 = aptutcend.toLocalDateTime();
+
+            int custid = appointments.customershm.get(customercombobox.getSelectionModel().getSelectedItem());
+            int userid = appointments.usershm.get(usercombobox.getSelectionModel().getSelectedItem());
+            int contactid = appointments.contactshm.get(contactcombobox.getSelectionModel().getSelectedItem());
+
+            HelloController.connection.createStatement().execute(
+                   "Insert into appointments values(" + colv + ", '" + titletxtfld.getText() +
+                           "', '" + desctxtfld.getText() + "', '" + loctxtfld.getText() + "', '" +
+                           typetxtfld.getText() + "', '" + aptstart1 + "', '" + aptend1 +
+                           "', '" + "2022-08-30 17:02:46" + "', '" + "script" + "', '" +
+                           "2022-08-30 17:02:46" + "', '" + "script" + "', " + custid + ", " +
+                           userid + ", " + contactid + ");");
+        } catch (SQLException e) {System.out.println("SQL Error: " + e.getMessage());}
+    }
     public void initialize() {
         HashMap<String, String> apthm = getaptinfo(appointments.toalterid);
         titletxtfld.setText(apthm.get("title"));descriptiontxtfld.setText(apthm.get("description"));
