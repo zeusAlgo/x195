629e18d8ab2a26210eed5d79cb58e0d9627dc3c9
u <uuu@hotmail.com>
2022-09-21 10:40:10 -0500

    m

--- src/main/java/com/example/x195/aptmodify.java
+++ src/main/java/com/example/x195/aptmodify.java
@@ -1,4 +1,12 @@
 package com.example.x195;
 
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+
 public class aptmodify {
+    public ComboBox userComboBox;
+    public TextField titlecombobox;
+    public TextField locationcombobox;
+    public TextField typecombobox;
+    public TextField descriptioncombobox;
 }
