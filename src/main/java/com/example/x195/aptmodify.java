1046d91b4c35975be724620b2dddf6fd9dc8e5ca
u <uuu@hotmail.com>
2022-09-21 10:46:16 -0500

    m

--- src/main/java/com/example/x195/aptmodify.java
+++ src/main/java/com/example/x195/aptmodify.java
@@ -1,21 +1,13 @@
 package com.example.x195;
-
 import javafx.event.ActionEvent;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 
 public class aptmodify {
-    public ComboBox userComboBox;
-    public TextField titlecombobox;
-    public TextField locationcombobox;
-    public TextField typecombobox;
-    public TextField descriptioncombobox;
-    public ComboBox contactcombobox;
-    public ComboBox monthcombox;
-    public ComboBox daycombobox;
-    public ComboBox timescombobox;
-    public ComboBox customercombobox;
+    public ComboBox userComboBox;public TextField titlecombobox;
+    public TextField locationcombobox, typecombobox, descriptioncombobox;public ComboBox contactcombobox, monthcombox, daycombobox, timescombobox, customercombobox;
 
+    
 
     public void updateapt(ActionEvent actionEvent) {
     }
