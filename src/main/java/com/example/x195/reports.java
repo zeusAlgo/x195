bae6f98353816422ae82134be52eea453a4e12c9
u <uuu@hotmail.com>
2022-09-22 17:23:21 -0500

    m

--- src/main/java/com/example/x195/reports.java
+++ src/main/java/com/example/x195/reports.java
@@ -5,6 +5,7 @@ import javafx.scene.control.TextArea;
 public class reports {
     public TextArea textarea;
 
+    public 
     public void initialize() {
         textarea.setText("dlkfsjldjflsfl\nlksdjflajdlfkjsjdf\n");
     }
