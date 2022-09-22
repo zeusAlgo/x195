5522ed97b12595f9977c9703874ffb904bca5f4d
u <uuu@hotmail.com>
2022-09-22 17:32:48 -0500

    m

--- src/main/java/com/example/x195/reports.java
+++ src/main/java/com/example/x195/reports.java
@@ -1,8 +1,6 @@
 package com.example.x195;
-
 import javafx.event.ActionEvent;
 import javafx.scene.control.TextArea;
-
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.HashMap;
@@ -15,24 +13,14 @@ public class reports {
     }
 
     public void report1(ActionEvent actionEvent) {
-        HashMap<String, Integer> janhm = new HashMap<>();
-        HashMap<String, Integer> febhm = new HashMap<>();
-        HashMap<String, Integer> marhm = new HashMap<>();
-        HashMap<String, Integer> aprhm = new HashMap<>();
-        HashMap<String, Integer> mayhm = new HashMap<>();
-        HashMap<String, Integer> junehm = new HashMap<>();
-        HashMap<String, Integer> julhm = new HashMap<>();
-        HashMap<String, Integer> aughm = new HashMap<>();
-        HashMap<String, Integer> septhm = new HashMap<>();
-        HashMap<String, Integer> octhm = new HashMap<>();
-        HashMap<String, Integer> novhm = new HashMap<>();
-        HashMap<String, Integer> dechm = new HashMap<>();
+        HashMap<String, Integer> janhm = new HashMap<>(), febhm = new HashMap<>(), marhm = new HashMap<>(), aprhm = new HashMap<>(), mayhm = new HashMap<>(), junehm = new HashMap<>(), julhm = new HashMap<>(), aughm = new HashMap<>(), septhm = new HashMap<>(), octhm = new HashMap<>(), novhm = new HashMap<>(), dechm = new HashMap<>();
 
         try {
-
             ResultSet rs = HelloController.connection.createStatement().executeQuery(
                     "Select * from appointments");
+            int monthint = rs.get
         } catch (SQLException e) {System.out.println(e.getMessage());}
+
     }
 
     public void report2(ActionEvent actionEvent) {
