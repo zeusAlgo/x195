5c45ed436586fc7a541bea849d1e0a86c109ccc8
u <uuu@hotmail.com>
2022-09-23 10:28:51 -0500

    m

--- src/main/java/com/example/x195/Customer.java
+++ src/main/java/com/example/x195/Customer.java
@@ -8,19 +8,19 @@ public class Customer {
         this.id = id; this.name = name; this.address=address; this.postalcode=postalcode; this.divid=divid;
     }
 
-    public void setid(int id) {this.id=id;}
-    public void setname(String name) {this.name=name;}
-    public void setaddress(String address) {this.address=address;}
-    public void postalcode(String postalcode) {this.postalcode=postalcode;}
-    public void phone(String phone) {this.phone=phone;}
-    public void divid(int divid) {this.divid=divid;}
+    public void setId(int id) {this.id=id;}
+    public void setName(String name) {this.name=name;}
+    public void setAddress(String address) {this.address=address;}
+    public void setPostalcode(String postalcode) {this.postalcode=postalcode;}
+    public void setPhone(String phone) {this.phone=phone;}
+    public void setDivid(int divid) {this.divid=divid;}
 
-    public int getid() {return id;}
-    public String getname() {return name;}
-    public String getaddress() {return address;}
-    public String getpostalcode() {return postalcode;}
-    public String getphone() {return phone;}
-    public int getdivid() {return divid;}
+    public int getId() {return id;}
+    public String getName() {return name;}
+    public String getAddress() {return address;}
+    public String getPostalcode() {return postalcode;}
+    public String getPhone() {return phone;}
+    public int getDivid() {return divid;}
 
     //mapping between the db
     // get instance of customer with jdbc
