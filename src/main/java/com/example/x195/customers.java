0f745e3a04a8e3289d0ff3304420b500f3bdc905
u <uuu@hotmail.com>
2022-09-25 18:27:11 -0500

    m

--- src/main/java/com/example/x195/customers.java
+++ src/main/java/com/example/x195/customers.java
@@ -19,7 +19,6 @@ public class customers {
     @FXML public TableView<Customer> tblView;
     @FXML public TableColumn<Customer, String> customerIdCol, customerNameCol, customerAddressCol, customerPostalCodeCol, customerPhoneCol, customerDivIdCol;
     @FXML public final ObservableList<Customer> customerInfo = FXCollections.observableArrayList();
-
     public static int tomodifyid = 0;
 
     public void launch1(ActionEvent actionEvent) {
